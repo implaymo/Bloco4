e4fda8d668d153a274b4086d3093e8189a387fba
implaymo <goncalomoreirasilva@gmail.com>
2024-10-31 17:56:16 +0000

    Fix some exercises that werent giving what teacher asked.

--- src/main/java/org/example/Exercicio8/Exercicio8.java
+++ src/main/java/org/example/Exercicio8/Exercicio8.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 
 public class Exercicio8 {
 
-    public static ArrayList<Integer> exercicio8(int start, int end, int numberToCheck1, int numberToCheck2){
+    public static ArrayList<Integer> exercicio8(int start, int end, int[] numbers){
         ArrayList<Integer> multipleNumbers = new ArrayList<>();
         for (int i = start; i <= end; i++){
-            if (i % numberToCheck1 == 0 && i % numberToCheck2 == 0){
+            boolean checkNumber = checkIfMultiple(i, numbers);
+            if (checkNumber){
                 multipleNumbers.add(i);
             }
         }
         return multipleNumbers;
+    }
+
+    public static boolean checkIfMultiple(int numberToCheck, int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numberToCheck % numbers[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/org/example/Exercicio8/Exercicio8Test.java
+++ src/test/java/org/example/Exercicio8/Exercicio8Test.java
@@ -1,10 +1,16 @@
 package org.example.Exercicio8;
 
+import org.example.Exercicio12.Exercicio12_3;
 import org.example.Exercicio7.Exercicio7;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,13 +21,23 @@ class Exercicio8Test {
         new Exercicio8();
     }
 
-    @Test
-    void should_return_array_with_multiples_of_number_6() {
+    @ParameterizedTest
+    @MethodSource("arrayProvider")
+    void should_return_a_valid_sum(int start, int end,int[] arrayInput, ArrayList<Integer> expected) {
         // arrange
-        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(6,12));
         // act
-        ArrayList<Integer> result = Exercicio8.exercicio8(4, 12, 2, 3);
+        ArrayList<Integer> result = Exercicio8.exercicio8(start, end, arrayInput);
         // assert
         assertEquals(expected, result);
+    }
+
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider() {
+        return Stream.of(
+                Arguments.of(4, 12, new int[]{2, 3}, new ArrayList<>(List.of(6, 12))),
+                Arguments.of(4, 10, new int[]{2, 3}, new ArrayList<>(List.of(6))),
+                Arguments.of(1, 20, new int[]{2, 3}, new ArrayList<>(List.of(6, 12, 18)))
+        );
     }
 }
